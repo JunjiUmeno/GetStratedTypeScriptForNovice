df678dbc556b51fab6d4ae104316c5123aa91208
Junji Umeno <jumeno@gmail.com>
2019-03-05 22:19:10 +0900

    init

---
+++ src/scripts/file.js
@@ -0,0 +1,4 @@
+function displayTotalPerCustomer(person, total) {
+    var message = "Total for " + person + " is " + total;
+    document.getElementById("totalMessage").innerText = message;
+}
---
+++ src/scripts/file.ts
@@ -0,0 +1,4 @@
+function displayTotalPerCustomer(person: string, total: number) {
+    let message: string = "Total for " + person + " is " + total;
+    document.getElementById("totalMessage").innerText = message;
+}
